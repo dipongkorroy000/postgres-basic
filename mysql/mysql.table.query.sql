49403a4907679b19e6c78230f219a236970db08b
dipongkorroy000 <dipongkorroy000@gmail.com>
2026-08-03 16:59:50 +0600

    learn mysql & postgres

--- mysql/mysql.table.query.sql
+++ mysql/mysql.table.query.sql
@@ -1,16 +1,33 @@
+USE pdb
+
 SELECT * FROM countries
 
 SELECT * FROM employees
 
 SELECT * FROM departments
 
-SELECT * FROM employees WHERE department_id = 60
+SELECT first_name, email FROM employees
+
+SELECT first_name AS "First Name", email FROM employees
 
 SELECT first_name, salAry, salary + 10 FROM employees
 
+SELECT first_name, email, salary FROM employees ORDER BY salary DESC
+
+SELECT employee_id, first_name, email, salary FROM employees ORDER BY salary DESC LIMIT 10 OFFSET 10
+
+SELECT first_name, email FROM employees WHERE (department_id = 60 AND salary > 5000);
+
+SELECT * FROM employees WHERE last_name LIKE 'green%'
+
+SELECT * FROM employees WHERE last_name LIKE '%ee%'
+
+SELECT * FROM employees WHERE last_name LIKE '%ey'
+
+
+
 SELECT COUNT(salary) FROM employees
 
-USE pdb
 
 CREATE TABLE stu(
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,8 +41,6 @@ SELECT * FROM stu
 TRUNCATE TABLE stu
 
 DROP Table stu
-
-SELECT * FROM employees WHERE department_id != 20
 
 SELECT department_id, AVG(salary)
 FROM employees
